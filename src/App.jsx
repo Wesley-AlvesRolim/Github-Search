be5455e350aa792ce0e6bc92a3e6b2b5dcb92a53
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-03-30 10:54:06 -0300

    Feat: add Toastify component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routes from './Routes';
 import { GlobalStyles } from './utils/GlobalStyles';
+import { Toastify } from './components/Toastify/Index';
 
 function App() {
   return (
     <div className="app">
       <GlobalStyles />
+      <Toastify />
       <Routes />
     </div>
   );
