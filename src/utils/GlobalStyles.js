58b363a14a9458a60ef4e83dcd8ed71f19fbd32d
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-04-01 16:24:50 -0300

    Feat: responsive rem unit

--- src/utils/GlobalStyles.js
+++ src/utils/GlobalStyles.js
@@ -8,6 +8,16 @@ export const GlobalStyles = createGlobalStyle`
   box-sizing: border-box;
 }
 
+html{
+  @media(max-width: 1080px){
+    font-size: 93.75%;
+  }
+
+  @media(max-width: 720px){
+    font-size: 87.5%;
+  }
+}
+
 body{
   font-family: 'Roboto' ,sans-serif;
   color: ${lightGray};
