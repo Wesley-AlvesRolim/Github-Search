57f2b3a9781f4294554312c214bba98cc7aa58d5
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-04-08 13:42:24 -0300

    Feat: add Loading and RepositoriesList components to Repositories page

--- src/pages/Repositories/Index.jsx
+++ src/pages/Repositories/Index.jsx
@@ -1,6 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import axios from '../../services/axios';
+
+import { Loading } from '../../components/Loading/Index';
+import { RepositoriesList } from '../../components/RepositoriesList/Index';
+
 function Repositories() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [userRepositoriesData, setUserRepositoriesData] = useState({ data: [] });
+  const [userRepositoriesToShowToUser, setUserRepositoriesToShowToUser] = useState({ data: [] });
+
+  useEffect(() => {
+    (async function getData() {
+      try {
+        setIsLoading(true);
+        const userInParams = searchParams.toString().split('q=').join('');
+        const { data } = await axios.get(`${userInParams}/repos`);
+        setUserRepositoriesData({ data });
+        setUserRepositoriesToShowToUser({ data });
+        setTimeout(() => setIsLoading(false), 3000);
+      } catch {
+        navigate('/', { replace: true });
+        setIsLoading(false);
+      }
+    }());
+  }, []);
+
   return (
-    <h1>Repositórios</h1>
+    <>
+      <Loading isLoading={isLoading} />
+
+      <RepositoriesList userRepositoriesToShowToUser={userRepositoriesToShowToUser} />
+    </>
   );
 }
 
